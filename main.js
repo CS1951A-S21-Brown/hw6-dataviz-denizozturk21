5f4868d2e0db658f570f4136fa215ef7e39129e5
denizozturk21 <denizozturk21@github.com>
2021-03-30 23:17:55 -0400

    .

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Add your JavaScript code here
 const MAX_WIDTH = Math.max(1080, window.innerWidth);
 const MAX_HEIGHT = 70;
 const margin = {top: 40, right: 100, bottom: 40, left: 100};
@@ -11,16 +10,16 @@ let graph_2_width = (MAX_WIDTH / 2) - 10, graph_2_height = 500;
 let graph_3_width = MAX_WIDTH / 2, graph_3_height = 250;
 
 let g1 = d3.select("#graph1").append("svg");
-g1.attr("width", graph_1_width).attr("heigth", graph_1_height)
+g1.attr("width", graph_1_width).attr("height", graph_1_height)
 .attr("transform", `translate(${margin.left},${margin.top})`).append("g");
 
 
 let g2 = d3.select("#graph2").append("svg");
-g2.attr("width", graph_2_width).attr("heigth", graph_2_height+50)
+g2.attr("width", graph_2_width).attr("height", graph_2_height+50)
 .attr("transform", `translate(${margin.left},${margin.top})`).append("g");
 
 let g3 = d3.select("#graph3").append("svg");
-g3.attr("width", graph_3_width).attr("heigth", graph_3_height)
+g3.attr("width", graph_3_width).attr("height", graph_3_height)
 .append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
 //add title here
@@ -40,13 +39,14 @@ var sliderPlaceholder2 = 2020;
 
 let yearSlider = new Slider('#yearSlider',{reversed : true});
 
-yearSlider.on('slide', function(t){
+yearSlider.on('slideStop', function(t){
     //let temp = $('#yearSlider').val();
     if(t[0] != null) {
         sliderPlaceholder1 = parseInt(t[0]);
         sliderPlaceholder2 = parseInt(t[1]);
-        //console.log(sliderPlaceholder1);
     }
+    console.log(sliderPlaceholder1);
+
     var datum = [];
     d3.csv("/data/football.csv").then(function(games) {
         gSize = games.length;
@@ -85,31 +85,47 @@ yearSlider.on('slide', function(t){
         setData.sort((game1, game2) => game2.season - game1.season);
         setData = setData.slice(0,10);
         x_range = [0, graph_1_width];
-        y_range = [0, graph_1_height];
+   
         var xd = d3.scaleLinear().range(x_range);
-        var yd = d3.scaleBand().range(y_range);
+     
         
-        coloring.domain(setData.map(function(game) {return parseInt(game.games)}));
-        yd.domain(setData.map(function(game) {return parseInt(game.season)}));
+
         xd.domain([0, 1200]);
-        y_axis = g1.append("g");
-        y_axis.call(d3.axisLeft(yd));
+      
+        coloring.domain([0,1200]);
         //g1.append("g").call(d3.axisBottom(x_axis));
+        var yd = d3.scaleBand().padding(0.1)
+        .domain(setData.map(function(team) { return team.season}))
+        .range([0, graph_1_width]);
+    
+        
         
         console.log(setData);
         console.log("setdata above");
         bs = g1.selectAll("rect").data(setData);
-        g1.selectAll("text").data(setData).enter().append("text").text(function(game){return parseInt(game.games)})
-        .attr("y", function(game){console.log(parseInt(game.season)); return yd(parseInt(game.season))})
-        .attr("x",function(game){return xd(parseInt(game.games))})
-        bs.enter().append("rect")
-        .attr("y", function(game){return (yd(game.season)/10)+400})
+        ts = g1.selectAll("text").data(setData);
+        ts2 = g1.selectAll("text").data(setData);
+        
+        // g1.selectAll("text").data(setData).enter().append("text").text(function(game){return parseInt(game.games)})
+        // .attr("y", function(game){console.log(parseInt(game.season)); return yd(parseInt(game.season))})
+        // .attr("x",function(game){return xd(parseInt(game.games))})
+        bs.enter().append("rect").merge(bs)
+        .attr("y", function(game){return yd(game.season)})
         .attr("x",  xd(0))
-        .attr("height", function(game){graph_1_height - (yd(parseInt(game.season))/10)+400})
-        .attr("width",function(game){return xd(parseInt(game.games))})
+        // .attr("height", function(game){graph_1_height - (yd(parseInt(game.season))/10)+400})
+        .attr("height", yd.bandwidth())
+        .attr("width",function(game){return xd(game.games)})
         .attr("fill",function(game){return coloring(game.games)});
-        g1.selectAll("text").data(setData).exit().remove();
-        g1.selectAll("rect").data(setData).exit().remove();  
+        ts.enter().append("text").merge(ts).attr("y", function(game){console.log(game.season); return yd(game.season)+15}).text( function(game){ return game.games})
+        .attr("x",graph_1_width- margin.left );
+        ts2.enter().append("text").merge(ts2).attr("y", function(game){console.log(game.season); return yd(game.season)+15}).text( function(game){ return game.season})
+        .attr("x",0);
+        g1.append("text")
+      .attr("transform", `translate(${(graph_1_width - margin.left) / 2},
+                                          ${(15)})`)
+      .style("text-anchor", "middle")
+      .text("Number of Games by Year");
+        g1.exit().remove();
 })});
 
 d3.csv("/data/football.csv").then(function(games) {
@@ -189,22 +205,29 @@ d3.csv("/data/football.csv").then(function(games) {
     console.log(teams);
     teams = teams.slice(0,10);
     console.log(teamMap);
-    var x = d3.scaleLinear()
-    //.domain()
-    .range([ 0, graph_3_width -margin.left - margin.right ]).domain(teams.map(function(d){return (d[0])}));
-  g3.append("g")
-    .attr("transform", "translate(0," + graph_3_height + ")")
-    .call(d3.axisBottom(x));
+    
+
+  var x = d3.scaleBand().padding(0.1)
+        .domain(teams.map(function(team) { return team[0]}))
+        .range([0, graph_3_width - margin.left - margin.right]);
+    g3.append("g")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        
+        .style("text-anchor", "end");
+    
+
     console.log(teams[0][0]);
     console.log(teams[0][1]);
     console.log(teams[0][2]);
   // Add Y axis
-  var y = d3.scaleBand()
+  var y = d3.scaleLinear()
     //
-    .range([0, graph_3_height-margin.top-margin.bottom])
-    .domain(teams.map(function(d){ return d[1] }));
+    .range([0,graph_3_height-margin.top-margin.bottom])
+    .domain([0,d3.max(teams, function(d) { return d[1]; })]);
+    var y_axis = d3.axisLeft().scale(y).ticks(5);
   g3.append("g")
-    .call(d3.axisLeft(y));
+    .call(y_axis);
   var z = d3.scaleLinear()
     .domain([0, 1])
     .range([5, 30]);
@@ -213,30 +236,39 @@ d3.csv("/data/football.csv").then(function(games) {
     .range(d3.schemeSet2);
     console.log("teams below");
     console.log(teams);
+    
    /* g3.enter().append("circle").data(teams).transition().duration(1000)
     .attr("cx", function (d) { return x(Date.parse(d[0])); } )
     .attr("cy", function (d) { return y(parseInt(d[1])); } )
     .attr("r", 4);
     g3.exit().remove();*/
     
-
-  g3.append('g')
-    .selectAll("dot")
-    .data(teams)
-    .enter()
+bs = g3.selectAll("dot").data(teams).attr("transform", "translate(0," + graph_3_height-margin.top-margin.bottom + ")");
+  bs.enter()
     .append("circle")
+    .merge(bs)
       .attr("class", "bubbles")
       .attr("cx", function (d) { return x(d[0]) } )
       .attr("cy", function (d) { return y((d[1])); } )
       .attr("r", function (d) { return z(d[2]); } )
       .style("fill", function (d) { return myColor(d[2]); } );
     
+      g3.append("text")
+      .attr("transform", `translate(${(graph_3_width - margin.left - margin.right) / 2},
+                                          ${(graph_3_height - margin.top - margin.bottom) + 15})`)
+      .style("text-anchor", "middle")
+      .text("Country and Win Percentage Scatter Plot (Radius is proportional to opponent strength)");
+      ts = g3.selectAll("text").data(teams);
+      
     g3.exit().remove();
     //g3.selectAll("rect").data(teams).exit().remove();  
 
+  
 });
 
 d3.csv("/data/football.csv").then(function(games) {
+    let coloring2 = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl("#D0F2EB", "#131BC0"), 15));
     let teamMap = new Map();
     gSize = games.length;
     for(var index=0;index<gSize;index++){
@@ -287,27 +319,37 @@ d3.csv("/data/football.csv").then(function(games) {
     var x = d3.scaleBand().padding(0.1)
         .domain(teams.map(function(team) { return team[0]}))
         .range([0, graph_2_width - margin.left - margin.right]);
+    coloring2.domain([0,1]);
     g2.append("g")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", "translate(0," + graph_2_height + ")")
         .style("text-anchor", "end");
     var y = d3.scaleLinear().domain([0, 1])
-      .range([graph_2_height-margin.top-margin.bottom, 0])
+      .range([0,graph_2_height-margin.top-margin.bottom])
     ;
-    g2.append("g").call(d3.axisLeft(y));
-    g2.selectAll("rect").data(teams).enter()
-        .append("rect")
-        .attr("transform", `translate(${(graph_2_width - margin.left - margin.right) / 2},
-                                            ${(graph_2_height - margin.top - margin.bottom) + 15})`)
-        .attr("x", function(team) { return x(teams[0])})
-        .attr("y", function(team) { return (graph_2_height - margin.top - margin.bottom) - y(team[1])})
-        .attr("height", function(team) { return y(teams[1])})
-        .attr("width", x.bandwidth);
+    //g2.selectAll(".tick").attr("y", y(0));
+    g2.append("g").call(d3.axisLeft(y).ticks(5)).selectAll("text")
+    .style("text-anchor", "end");
+    bs = g2.selectAll("rect").data(teams).attr("transform", "translate(0," + graph_2_height + ")");
+    ts = g1.selectAll("text").data(teams);
+    //ts = g1.selectAll("rect").data(teams);
+    bs.enter()
+        .append("rect").merge(bs)
+        
+                .attr("x", function(d) { return x(d[0]); })
+                .attr("y", y(0))
+        .attr("height", function(team) { return y(team[1])})
+        .attr("width", x.bandwidth())
+        .attr("fill", function(team) { return coloring2(team[1])});
     g2.append("text")
         .attr("transform", `translate(${(graph_2_width - margin.left - margin.right) / 2},
                                             ${(graph_2_height - margin.top - margin.bottom) + 15})`)
         .style("text-anchor", "middle")
-        .text("Count");
+        .text("Countries and Their Winning Percentages");
+        //ts.enter().append("text").merge(ts).attr("x", function(game){return x(game[0])}).text( function(game){ return game[1].toFixed(2)})
+        //.attr("y",  y(0);
     g2.exit().remove();
+
+ 
 });
